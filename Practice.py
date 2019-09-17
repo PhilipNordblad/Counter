ee226c7f5a028bd0c8154213b368962a55b45b86
Philip Nordblad <philipnordblad@dpengineering.org>
2019-09-17 13:10:47 -0700

    Click finally works

---
+++ Practice.py
@@ -0,0 +1,32 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+
+
+
+class MyGrid(Widget):
+
+
+    count = 0
+
+    def btn(self):
+
+        self.count = self.count+1
+        print("Clicks: ", self.count)
+
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
